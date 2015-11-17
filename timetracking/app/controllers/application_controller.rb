a4825608898f7c2c191adaf94b1d02290de79cca
Lara Hoya <larascully@hotmail.com>
2015-11-17 09:45:48 +0100

    Not found page

--- timetracking/app/controllers/application_controller.rb
+++ timetracking/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  
+  def render_404(params)
+    Rails.logger.warn("Tried to access #{params} which did not exist.")
+    render "layouts/404"
+  end
+
   protect_from_forgery with: :exception
 end
--- timetracking/app/controllers/projects_controller.rb
+++ timetracking/app/controllers/projects_controller.rb
@@ -1,7 +1,11 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.last_created_projects(5)
+    @projects = Project.last_created_projects(10)
+  end
+
+  def show
+    @project = Project.find_by(id: params[:id]) || render_404(params)
   end
 
 end
